683d93d07a8920dccddcc235b9c25782c69c4e7a
rlghks1 <65458038+rlghks1@users.noreply.github.com>
2023-02-02 13:54:12 +1100

    edited naverCrawling.py for better quality

--- naverShopping/naverCrawling.py
+++ naverShopping/naverCrawling.py
@@ -8,7 +8,7 @@ import csv
 client_id = "WXRkOl6rH6gf4xRCQM_I"
 client_secret = "ypJ1utK1SB"
 
-display = "3"
+display = "100"
 encText = urllib.parse.quote("닭가슴살 볶음밥")
 url = "https://openapi.naver.com/v1/search/shop?query=" + encText + "&display=" + display # JSON result
 # url = "https://openapi.naver.com/v1/search/blog.xml?query=" + encText # result
@@ -22,22 +22,21 @@ if(rescode==200):
     #print(response_body.decode('utf-8'))
     with open('protein_meal_naver.csv', 'w', newline = '') as output_file :
         data = json.loads(response_body)
-        print(data['items'])
         f = csv.writer(output_file)
         f.writerow(["title","link","image","lprice","hprice","mallName",
                     "productId","productType","brand","maker","category1","category2","category3","category4"])
         
         # no save if the category of product is not 'meal' type
-        
-        matches = ['도시락']
+        #matches = ['밥','볶음밥']
         
         for datum in data['items']:
-            print(datum)
-            categories = [datum["category1"],datum["category2"],datum["category3"],datum["category4"]]
+            category = [datum["title"]]
+            print(category)
+            
             # exclude product which is not a meal
-            if any(x not in categories for x in matches):
+            #if any(x not in category for x in matches):
                 
-                continue
+            #    continue
             
             f.writerow([datum["title"],datum["link"],datum["image"],datum["lprice"],datum["hprice"],datum["mallName"], 
                         datum["productId"],datum["productType"],datum["brand"],datum["maker"],datum["category1"],
